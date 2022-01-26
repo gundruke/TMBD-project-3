2c054f3a6505f81737f7b089406fdefded74dd4d
alexandradecarvalho <alexandracarvalho@ua.pt>
2022-01-26 18:00:17 +0000

    Changed layout and think I fixed the evaluation metric code (?)

--- notebooks/evaluation_metrics.py
+++ notebooks/evaluation_metrics.py
@@ -3,23 +3,24 @@ from sklearn.metrics.pairwise import euclidean_distances
 import pandas as pd
 import numpy as np
 
-def neighborhood_preservation_trustworthiness(k, som, inpt):
+def neighborhood_preservation_trustworthiness(k, inpt, som):
 
-    # number of neNeighbourhooighbours k should be < data size // 2 
+    # number of neighbours, k, should be < data size // 2 
     n = inpt.shape[0]
     assert k < (n / 2), 'k must be < N/2.'
     
-    d = euclidean_distances(inpt, som) # calculating euclidean distances between input and som
-    d_data = euclidean_distances(inpt) + np.diag(np.inf * np.ones(n))
-    print(d_data)
-    projections = som[np.argmin(d, axis=1)]
-    d_projections = euclidean_distances(projections) + np.diag(np.inf * np.ones(n))
+    #d = euclidean_distances(inpt, som) # calculating euclidean distances between input and som
+    #projections = som[np.argmin(d, axis=1)] # vector with smallest distance for each observation
     
-    original_ranks = pd.DataFrame(d_data).rank(method='min', axis=1)
-    projected_ranks = pd.DataFrame(d_projections).rank(method='min', axis=1)
+    d_data = euclidean_distances(inpt) + np.diag(np.inf * np.ones(n))        # calculating distances on the input
+    d_projections = euclidean_distances(som) + np.diag(np.inf * np.ones(n))  # calculating distances on the som
+    
+    original_ranks = pd.DataFrame(d_data).rank(method='min', axis=1)         # computing ranks from lowest in group to max
+    projected_ranks = pd.DataFrame(d_projections).rank(method='min', axis=1) # computing ranks from lowest in group to max
     
     weights = (projected_ranks <= k).sum(axis=1) / (original_ranks <= k).sum(axis=1)  # weight k-NN ties
     
+    # comparing both neighbourhoods
     nps = np.zeros(n)
     trs = np.zeros(n)
     for i in range(n):
